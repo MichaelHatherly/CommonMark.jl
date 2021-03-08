e78d0f629ec57b643368be7aa126862fec768075
Mike <michaelhatherly@gmail.com>
2021-03-08 22:29:09 +0000

    Naming changes, docs, and printing fixes.

--- src/extensions/interpolation.jl
+++ src/extensions/interpolation.jl
@@ -9,17 +9,19 @@
 struct JuliaValue <: AbstractInline
     ex
     ref::Ref{Any}
-    JuliaValue(ex) = new(ex, Ref{Any}())
+    JuliaValue(ex) = new(ex, Ref{Any}(nothing))
 end
 
-struct JuliaInterpolation
+# This rule should only be used from the exported `@cm_str` macro and not
+# `enabled!` directly by users on a `Parser` object.
+struct JuliaInterpolationRule
     captured::Vector{JuliaValue}
-    JuliaInterpolation() = new(JuliaValue[])
+    JuliaInterpolationRule() = new(JuliaValue[])
 end
 
 const reInterpHere = r"^\$"
 
-inline_rule(ji::JuliaInterpolation) = Rule(1, "\$") do p, node
+inline_rule(ji::JuliaInterpolationRule) = Rule(1, "\$") do p, node
     dollar = match(reInterpHere, p)
     if dollar === nothing || length(dollar.match) > 1
         return false
@@ -43,8 +45,65 @@ end
 
 export @cm_str
 
+"""
+    cm""
+
+A string macro for markdown text that implements standard string interpolation.
+Returns a parsed markdown AST with the values of the interpolation expressions
+embedded in the AST.
+
+```julia
+value = "interpolated"
+cm"Some *\$(value)* text."
+```
+
+The default syntax rules used for parsing are:
+
+  - `AdmonitionRule`
+  - `AttributeRule`
+  - `AutoIdentifierRule`
+  - `CitationRule`
+  - `FootnoteRule`
+  - `MathRule`
+  - `RawContentRule`
+  - `TableRule`
+  - `TypographyRule`
+
+which matches closely with the default syntax supported in `Markdown.@md_str`.
+
+!!! info
+
+    The `DollarMathRule` is not enabled since it conflicts with the
+    interpolation syntax. Use double backticks and `math` language literal
+    blocks for maths that is provided by the `MathRule`.
+
+A custom `Parser` can be invoked when using `cm""` by providing a suffix
+to the macro call, for example:
+
+```julia
+more = "more"
+cm"Some **\$(uppercase(more))** text."none
+```
+
+where the suffixed `none` will invoke a basic `Parser` with no additional
+syntax rules `enabled!`. To use your own custom parser, for example to only
+enable the `TypographyRule`, you can suffix the call with a named function from
+the current module's global scope that returns the `Parser` object with the
+required rules enabled:
+
+```julia
+custom() = enable!(Parser(), TypographyRule())
+```
+
+It can then be used as
+
+```julia
+str = "custom"
+cm"A '\$(titlecase(str))' parser..."custom
+```
+"""
 macro cm_str(str, name = "jmd")
-    ji = JuliaInterpolation()
+    ji = JuliaInterpolationRule()
     parser = _init_parser(__module__, name)
     enable!(parser, ji)
     ast = parser(str)
@@ -58,7 +117,7 @@ function _interp!(ast::Node, refs::Vector, values::Vector)
     return ast
 end
 
-function _init_parser(mod::Module, name::AbstractString)
+function _init_parser(mod::Module, name::AbstractString)::Parser
     options = (
         jmd = function ()
             p = Parser()
@@ -75,11 +134,11 @@ function _init_parser(mod::Module, name::AbstractString)
             ])
             return p
         end,
-        cm = () -> Parser()
+        none = () -> Parser()
     )
     s = Symbol(name)
     if isdefined(mod, s)
-        obj = getfield(mod, s)()
+        obj = getfield(mod, s)
         if isa(obj, Function)
             try
                 p = obj()
@@ -88,7 +147,7 @@ function _init_parser(mod::Module, name::AbstractString)
             end
         end
     end
-    return get(options, s, Parser)()::Parser
+    return get(options, s, Parser)()
 end
 
 #
@@ -105,12 +164,12 @@ function write_latex(jv::JuliaValue, rend, node, enter)
     print(rend.buffer, sprint(_showas, MIME("text/latex"), jv.ref[]))
 end
 
-_showas(io::IO, m::MIME, obj) = showable(m, obj) ? show(io, m, obj) : show(io, obj)
+_showas(io::IO, m::MIME, obj) = showable(m, obj) ? show(io, m, obj) : print(io, obj)
 
 function write_term(jv::JuliaValue, rend, node, enter)
     style = crayon"yellow"
     push_inline!(rend, style)
-    print_literal(rend, style, sprint(show, jv.ref[]), inv(style))
+    print_literal(rend, style, sprint(print, jv.ref[]), inv(style))
     pop_inline!(rend)
 end
 
