801ea5dfec6537e25f6384f6a8e14867b139af5a
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-04 15:59:55 +0100

    precompile.jl: enable all extension rules

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,7 +2,22 @@ using SnoopPrecompile
 
 @precompile_all_calls begin
     parser = Parser()
-    enable!(parser, FootnoteRule())
+    # Enable all non-default (i.e. extension) rules
+    for rule in [
+        AdmonitionRule,
+        AttributeRule,
+        AutoIdentifierRule,
+        CitationRule,
+        DollarMathRule,
+        FootnoteRule,
+        FrontMatterRule,
+        MathRule,
+        RawContentRule,
+        TableRule,
+        TypographyRule,
+    ]
+        enable!(parser, rule())
+    end
     ast = parser("Hello *world*")
     for writer in [html, latex, term, markdown, notebook]
         writer(ast)
