a6e302f6c53bfac7b9756b34e256e7cf7f1924af
MichaelHatherly <michaelhatherly@gmail.com>
2025-06-03 00:52:28 +0100

    Convert unicodes tests to reference tests

--- test/unicodes.jl
+++ test/unicodes.jl
@@ -1,7 +1,27 @@
 @testset "Unicodes" begin
+    using ReferenceTests
+
+    # Helper function for tests that can use references
+    function test_unicode(base_name, ast)
+        formats = [
+            (html, "html.txt"),
+            (latex, "tex"),
+            (markdown, "md"),
+            (term, "txt"),
+            (typst, "typ"),
+        ]
+        for (func, ext) in formats
+            filename = "references/unicodes/$(base_name).$(ext)"
+            output = func(ast)
+            @test_reference filename Text(output)
+        end
+    end
+
     p = Parser()
     enable!(p, AdmonitionRule())
+
+    # Unicode in admonition title
     text = "!!! note \"Ju 的文字\"\n    Ju\n"
-    @test html(p(text)) ==
-          "<div class=\"admonition note\"><p class=\"admonition-title\">Ju 的文字</p>\n<p>Ju</p>\n</div>"
+    ast = p(text)
+    test_unicode("admonition_unicode_title", ast)
 end
