1ccfe2ed3a3e901305f6d799deb1ac0dd89a7967
MichaelHatherly <michaelhatherly@gmail.com>
2025-06-03 23:37:38 +0100

    Only check ambiguities are newer versions

--- test/ambiguities.jl
+++ test/ambiguities.jl
@@ -2,5 +2,8 @@
     using CommonMark
     using Test
 
-    @test isempty(Test.detect_ambiguities(Base, Core, CommonMark; recursive = true))
+    # Only run on newer Julia versions.
+    if VERSION > v"1.6"
+        @test isempty(Test.detect_ambiguities(Base, Core, CommonMark; recursive = true))
+    end
 end
